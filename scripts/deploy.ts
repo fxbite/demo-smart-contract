fde93dacde901ac155801ff11e33f61d21172f33
fxbite <letruongthinh1703@gmail.com>
2023-07-29 02:59:31 +0000

    Deploy smart contract on marketplace

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,18 +7,29 @@ async function main() {
   const [deployer] = await ethers.getSigners();
   console.log('deploy from address: ', deployer.getAddress());
 
+  // const Floppy = await ethers.getContractFactory("Floppy");
+  // const floppy = await Floppy.deploy();
+  // const floopyAddress = await floppy.getAddress()
+  // console.log('Floppy address: ', floppyAddress);
+  // Config.setConfig(network + '.Floppy', floopyAddress);
 
-  const Floppy = await ethers.getContractFactory("Floppy");
-  const floppy = await Floppy.deploy();
-  console.log('Floppy address: ', floppy.getAddress());
-  const floopyAddress = await floppy.getAddress()
-  Config.setConfig(network + '.Floppy', floopyAddress);
+  // const Vault = await ethers.getContractFactory("Vault");
+  // const vault = await Vault.deploy();
+  // const vaultAddress = await vault.getAddress()
+  // console.log('Floppy address: ', vaultAddress);
+  // Config.setConfig(network + '.Vault', vaultAddress);
 
-  const Vault = await ethers.getContractFactory("Vault");
-  const vault = await Vault.deploy();
-  console.log('Floppy address: ', vault.getAddress());
-  const vaultAddress = await vault.getAddress()
-  Config.setConfig(network + '.Vault', vaultAddress);
+  // const Hero = await ethers.getContractFactory("Hero");
+  // const hero = await Hero.deploy();
+  // const heroAddress = await hero.getAddress()
+  // console.log('stman hero address: ', heroAddress);
+  // Config.setConfig(network + '.Hero', heroAddress);
+
+  // const MKP = await ethers.getContractFactory("HeroMarketplace");
+  // const heroMarketplace = await MKP.deploy("0x038F556b3E9B5D16f792cB670263105d0eaf7E28", "0xcDc11cE81E70437098B31d0695ECA69AF16E8714");
+  // const heroMarketplaceAddress = await heroMarketplace.getAddress()
+  // console.log('Market deployed at: ', heroMarketplaceAddress);
+  // Config.setConfig(network + '.HeroMarketplace', heroMarketplaceAddress);
 
   await Config.updateConfig()
 }
